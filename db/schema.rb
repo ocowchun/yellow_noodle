14dee660962676ada934c394648cf6642a0052c8
ocowchun <ocowchun@gmail.com>
2014-06-01 13:14:50 +0800

    New: CreateRefDims

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140601051341) do
+ActiveRecord::Schema.define(version: 20140601051435) do
 
   create_table "action_dims", force: true do |t|
     t.string   "action_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ref_dims", force: true do |t|
+    t.string   "referer_url"
+    t.string   "event_name"
+    t.string   "platform_name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
